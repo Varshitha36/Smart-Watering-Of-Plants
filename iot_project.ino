8743acc93944384e9cab149accfadebc8aaf502b
Varshitha36 <153372585+Varshitha36@users.noreply.github.com>
2024-11-02 14:40:26 +0530

    Update iot_project.ino

--- iot_project.ino
+++ iot_project.ino
@@ -4,22 +4,27 @@ int sensor_pin = A0;
 
 void setup()
 
-{ Serial.begin(9600);
+{ 
+ 
+ Serial.begin(9600);
  pinMode(relayPin, OUTPUT);
   pinMode(sensor_pin, INPUT); 
   Serial.println ("Reading From the Sensor..."); 
   delay(2000);
-  }
+ 
+}
 
 void loop()
 
-{ output_value = analogRead(sensor_pin);
+{ 
+ output_value = analogRead(sensor_pin);
  output_value =map (output_value, 550, 10, 0, 100);
-  Serial.print ("Moisture: ");
-   Serial.print (output_value); 
-   Serial.println("%");
+ Serial.print ("Moisture: ");
+ Serial.print (output_value); 
+ Serial.println("%");
     if (output_value <20 )
-    { digitalWrite (relayPin, LOW);
+    { 
+     digitalWrite (relayPin, LOW);
 
 }
 
